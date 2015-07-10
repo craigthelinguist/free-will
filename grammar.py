5bf5a43f19ef7142f49a824769fefd964312f6a8
Aaron Craig <aaron@nzrs.net.nz>
2015-07-10 15:27:19 +1200

    fixed infix -> postfix transformer; now generates ASTs for arbitrary rules

--- grammar.py
+++ grammar.py
@@ -4,22 +4,33 @@ from re import split
 from collections import defaultdict
 
 def compile(grammar):
+    '''
+    Compile the specified grammar into a dict of rule name -> abstract syntax tree.
+    :param grammar: string input, with rules delimited by new lines
+    :return: { str : (nested tuples) )
+    '''
     lines = grammar.split("\n")
     rules = {}
     for line in lines:
         line = grammar.split(":=")
         name = line[0].lstrip().rstrip()
         body = line[1].lstrip().rstrip()
-        rules[name] = sexpr(body)
+        rules[name] = ast(body)
     return rules
 
 def transform(str):
+    '''
+    Transform string representation of primitive compositions into the representation
+    used internally by the AST. E.g.: "|" --> "OR"
+    :param str: string to transform
+    :return: string
+    '''
     if str == "|": return "OR"
     elif str.startswith('"') and str.endswith('"'):
         return "LITER " + str[1:-1]
     else: return str
 
-def infix2prefix(infix):
+def infix2postfix(infix):
     '''
     Transform infix representation of a rule to its postfix representation.
     :param infix: s-expression (list of string tokens)
@@ -27,36 +38,35 @@ def infix2prefix(infix):
     '''
 
     # Establish precedence of operators.
-    precedence = defaultdict(lambda : 1)
-    precedence["AND"] = 3
-    precedence["OR"] = 2
-    OPERS = ["AND", "OR"]
+    opers = { "AND" : 3, "OR" : 2 }
 
     # Set up stack and infix/postfix strings.
-    stack = [")"]
+    infix.reverse()
     infix.append("(")
-    prefix = []
+    stack = [")"]
+    postfix = []
 
     for token in infix:
 
         if token == ")":
-            stack.push(token)
+            stack.append(token)
 
-        elif token in OPERS:
-            prec = precedence[token]
-            while len(stack) > 0 and stack[-1] in OPERS and precedence[stack[-1]] <= prec:
-                prefix.append(stack.pop())
-            prefix.append(token)
+        elif token in opers:
+            prez = opers[token]
+            while len(stack) > 0 and stack[-1] in opers and opers[stack[-1]] >= prez:
+                postfix.append(stack.pop())
+            stack.append(token)
 
-        elif token == ")":
-            while stack[-1] in OPERS:
-                prefix.append(stack.pop())
+        elif token == "(":
+            while len(stack) > 0 and stack[-1] in opers:
+                postfix.append(stack.pop())
+            stack.pop()
 
-        else: prefix.append(token)
+        else: postfix.append(token)
 
-    return prefix
+    return postfix
 
-def sexpr(body):
+def ast(body):
     '''
     Turn a rule-body into its corresponding s-expression.
     :param body: string
@@ -72,13 +82,22 @@ def sexpr(body):
         token = transform(token)
         newbody.append(token)
         prev = token
+
+    # Turn infix string representation to postfix string representation.
     postfix = infix2postfix(newbody)
     astuple = lambda x : tuple(x.split(" "))
-    return [astuple(x) for x in postfix]
+    postfix = [astuple(x) for x in postfix]
 
-def main():
-    g = 'GEO := "wellington" NUM | "auckland" NUM'
-    print compile(g)
+    # Turn the tuples into an abstract syntax tree.
+    stack = []
+    for rule in postfix:
+        if rule not in [("AND",), ("OR",)]:
+            stack.append(rule)
+        else:
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            tree = (rule[0], arg1, arg2)
+            stack.append(tree)
 
-if __name__ == "__main__":
-    main()
+    if len(stack) != 1: raise ValueError("Final AST should only have one thing in it :(")
+    return stack[0]
